11c92a39127da20ca5454edb8099c8866619765e
MUneeb A <muneebarif11@gmail.com>
2021-07-05 16:05:29 +0500

    Using custom validations

--- form.js
+++ form.js
@@ -1,12 +1,15 @@
 function validateform(){  
-var name=document.myform.name.value;  
-var password=document.myform.password.value;  
-  
-if (name==null || name==""){  
-  document.getElementById('demo').innerHTML = "Please Enter your Name";   
-  return false;  
-}else if(password.length<6){  
-  alert("Password must be at least 6 characters long.");  
-  return false;  
-  } 
+	var name=document.myform.fname.value;  
+	var password=document.myform.password.value;  
+	  
+	  // debugger;
+
+	if (name==null || name==""){ 
+		$('#demo').html("Please Enter your Name"); 
+	  // document.getElementById('demo').innerHTML = "Please Enter your Name";   
+	  return false;  
+	}else if(password.length<6){  
+	  alert("Password must be at least 6 characters long.");  
+	  return false;  
+	} 
 }   
