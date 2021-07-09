8db5e233cdf754c721cf2d2b0ff5295ba4dab3ac
Abdul Rehman <mrrehman217@gmail.com>
2021-07-09 16:24:34 +0500

    Form Validate

--- form.js
+++ form.js
@@ -1,15 +1,62 @@
-function validateform(){  
-	var name=document.myform.fname.value;  
-	var password=document.myform.password.value;  
-	  
-	  // debugger;
+$("#profile-form").validate({
+	rules: {
+		first_name: "required",
+		last_name: "required",
+		email: {
+			required: true,
+			email: true,
+		},
+		password: {
+			required: true,
+			minlength: 6
+		},
+		confrim_pass: {
+			required: true,
+			minlength: 6,
+			equalTo: "#password"
+		},
+		phoneN: {
+			required: true,
+			maxlength: 13,
+		},
+		home_add: {
+			required: true,
+			maxlength: 4,
+		},
+		aboutme: {
+			required: true,
+			maxlength: 100,
+		},
+	},
+	messages: {
+		first_name: "Please Enter your First Name",
 
-	if (name==null || name==""){ 
-		$('#demo').html("Please Enter your Name"); 
-	  // document.getElementById('demo').innerHTML = "Please Enter your Name";   
-	  return false;  
-	}else if(password.length<6){  
-	  alert("Password must be at least 6 characters long.");  
-	  return false;  
-	} 
-}   
+		last_name: "Please Enter your Last Name",
+
+		email: {
+			required: "Please enter your email",
+			email: "Please enter valid email",
+		},
+		password: {
+			required: "Please enter your password",
+			minlength: "Your password must be at least 6 characters long",
+		},
+		confrim_pass: {
+			required: "Please provide a password",
+			minlength: "Your password must be at least 6 characters long",
+			equalTo: "Please enter the same password as above",
+		},
+		phoneN: {
+			required: "Please enter your Phone Number",
+			maxlength: "Your password must be at least 13 number long",
+		},
+		home_add: {
+			required: "Please enter your Address",
+			maxlength: "Your address should be at least 4 characters long",
+		},
+		aboutme: {
+			required: "Please leave a message.",
+			maxlength: "Only 100 characters allow",
+		},
+	}
+});  
